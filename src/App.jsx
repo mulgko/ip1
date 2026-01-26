410e577f81a14f5b0cb34b0ab50dae43b5c1927e
u-ulim <uiissoop@gmail.com>
2026-01-26 14:41:50 +0900

    zustand, clsx install

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import NavBar from './components/Navbar'
 import Hero from './components/Hero'
+import ProductViewer from './components/ProductViewer'
 
 const App = () => {
   return (
     <main>
       <NavBar />
       <Hero />
+      <ProductViewer />
     </main>
   )
 }
