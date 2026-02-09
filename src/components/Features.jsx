81bfdac3fc855111ba0c999d1846d6276d593988
u-ulim <uiissoop@gmail.com>
2026-02-09 13:50:26 +0900

    section

---
+++ src/components/Features.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Features = () => {
+    return (
+        <div className="mt-auto w-40 border-amber-700 border-2">
+
+        </div>
+    )
+}
+
+export default Features
